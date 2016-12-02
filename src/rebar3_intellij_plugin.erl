e5bce8a06fae315fd2bd1f3b18fcf6b425cd3d8d
bryan hunt <github@binarytemple.co.uk>
2016-12-02 00:10:18 +0000

    wip - spitting out source files listing to /tmp now

--- src/rebar3_intellij_plugin.erl
+++ src/rebar3_intellij_plugin.erl
@@ -35,11 +35,16 @@
 -behaviour(provider).
 
 -export([init/1,
-         do/1,
-         format_error/1]).
+  do/1,
+  format_error/1]).
 
 -define(PROVIDER, generate).
--define(DEPS, [{default, compile}]).
+-define(DEPS, [{default,
+
+
+  app_discover, lock,
+
+  compile}]).
 
 %% ===================================================================
 %% Public API
@@ -47,31 +52,61 @@
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    State1 = rebar_state:add_provider(State, providers:create([{name, ?PROVIDER},
-                                                               {module, ?MODULE},
-                                                               {namespace, intellij},
-                                                               {bare, false},
-                                                               {deps, ?DEPS},
-                                                               {example, "rebar3 intellij compile"},
-                                                               {short_desc, "generate intellij config files."},
-                                                               {desc, "generate intellij config files."},
-                                                               {opts, [intellij]}])),
+  State1 = rebar_state:add_provider(State, providers:create([{name, ?PROVIDER},
+    {module, ?MODULE},
+    {namespace, intellij},
+    {bare, false},
+    {deps, ?DEPS},
+    {example, "rebar3 intellij compile"},
+    {short_desc, "generate intellij config files."},
+    {desc, "generate intellij config files."},
+    {opts, [intellij]}])),
 
-file:write_file("/tmp/state.txt",[io_lib:format("~p",[State1])]),
-file:write_file("/tmp/tag.txt",  [io_lib:format("~p",[os:timestamp()])]),
-file:write_file("/tmp/deps.txt", [io_lib:format("~p",[rebar_state:code_paths(State1, all_plugin_deps)])]),
-file:write_file("/tmp/args.txt", [io_lib:format("~p",[rebar_state:command_parsed_args(State1)])]),
-file:write_file("/tmp/apps.txt", [io_lib:format("~p",[rebar_state:project_apps(State1)])]),
+  file:write_file("/tmp/state.txt", [io_lib:format("~p", [State1])]),
+  file:write_file("/tmp/tag.txt", [io_lib:format("~p", [os:timestamp()])]),
+  file:write_file("/tmp/deps.txt", [io_lib:format("~p", [rebar_state:code_paths(State1, all_plugin_deps)])]),
+  file:write_file("/tmp/args.txt", [io_lib:format("~p", [rebar_state:command_parsed_args(State1)])]),
+%% file:write_file("/tmp/apps.txt", [io_lib:format("~p",[rebar_state:project_apps(State1)])]),
+%%file:write_file("/tmp/deps.txt", [io_lib:format("~p",[rebar_state:code_paths (State1)])]),
+  Apps = case rebar_state:current_app(State1) of
+           undefined ->
+             rebar_state:project_apps(State1);
+           AppInfo ->
+             [AppInfo]
+         end,
+  file:write_file("/tmp/apps.txt", [io_lib:format("~p", [Apps])]),
+
+
+  Res = [begin
+           Opts = rebar_app_info:opts(AppInfo),
+           OutDir = rebar_app_info:out_dir(AppInfo),
+           Dir = rebar_app_info:dir(AppInfo),
+           ErlFiles = rebar_utils:find_files(Dir, ".*\\.erl\$"),
+           HrlFiles = rebar_utils:find_files(Dir, ".*\\.hrl\$"),
+           RebarConfigFiles = rebar_utils:find_files(Dir, "rebar\\.config\$"),
+
+           [
+             {opts, Opts},
+             {outdir, OutDir},
+             {sourcedir, Dir},
+             {erl_files, ErlFiles},
+             {hrl_files, HrlFiles},
+             {app_config_files, RebarConfigFiles}
+           ]
+         end || AppInfo <- Apps],
+  file:write_file("/tmp/res.txt", [io_lib:format("~p", [Res])]),
+
+
 %%file:write_file("/tmp/dir.txt",  [io_lib:format("~p",[rebar_dir:recursive(rebar_state:opts(State1,"foo"))])]),
-    {ok, State1}.
+  {ok, State1}.
 
 do(State) ->
-    rebar_api:info("Running intellij...", []),
-    {ok, State}.
+  rebar_api:info("Running intellij...", []),
+  {ok, State}.
 
--spec format_error(any()) ->  iolist().
+-spec format_error(any()) -> iolist().
 format_error(Reason) ->
-    io_lib:format("~p", [Reason]).
+  io_lib:format("~p", [Reason]).
 
 %% ===================================================================
 %% Internal functions
